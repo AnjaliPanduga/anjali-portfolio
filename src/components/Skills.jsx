f3384b9b4e977a596ea488e2374bfd5ffc990be3
ANJALI PANDUGA <pandugaanjali2003@gmail.com>
2026-01-30 19:18:28 +0530

    Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,17 +1,31 @@
+import { motion } from "framer-motion";
+
 export default function Skills() {
   return (
-    <section id="skills">
+    <motion.section
+      id="skills"
+      initial={{ opacity: 0, y: 60 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.9 }}
+    >
       <h2>Skills</h2>
+
       <div className="grid">
         <span>Python</span>
         <span>SQL</span>
         <span>Pandas</span>
         <span>NumPy</span>
+        <span>Matplotlib</span>
+        <span>Seaborn</span>
         <span>Scikit-learn</span>
-        <span>Streamlit</span>
+        <span>Machine Learning</span>
         <span>EDA</span>
         <span>Statistics</span>
+        <span>Streamlit</span>
+        <span>Git & GitHub</span>
       </div>
-    </section>
+    </motion.section>
   );
 }
+
