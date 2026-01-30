9eaa82597b8bcf6b91a2ec0b605e331b9e1601f8
ANJALI PANDUGA <pandugaanjali2003@gmail.com>
2026-01-30 19:19:28 +0530

    Certifications.jsx

--- src/components/Certifications.jsx
+++ src/components/Certifications.jsx
@@ -1,12 +1,20 @@
+import { motion } from "framer-motion";
+
 export default function Certifications() {
   return (
-    <section id="certifications">
+    <motion.section
+      id="certifications"
+      initial={{ opacity: 0, y: 60 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.9 }}
+    >
       <h2>Certifications</h2>
       <ul>
-        <li>NASSCOM – Data Science Internship</li>
-        <li>HackerRank – SQL Advanced</li>
-        <li>HackerRank – Python</li>
+        <li>📜 Virtual Internship in Data Science – NASSCOM</li>
+        <li>📜 SQL (Basic, Intermediate & Advanced) – HackerRank</li>
+        <li>📜 Python (Basic) – HackerRank</li>
       </ul>
-    </section>
+    </motion.section>
   );
 }
