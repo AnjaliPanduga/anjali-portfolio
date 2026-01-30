04c3b1b7cf961251470f60065e9dbfe51d680c06
ANJALI PANDUGA <pandugaanjali2003@gmail.com>
2026-01-30 19:12:07 +0530

    Contact.jsx

---
+++ src/components/Contact.jsx
@@ -0,0 +1,10 @@
+export default function Contact() {
+  return (
+    <section id="contact">
+      <h2>Contact</h2>
+      <p>Email: pandugaanjali2003@gmail.com</p>
+      <p><a href="https://github.com/AnjaliPanduga">GitHub</a></p>
+      <p><a href="https://www.linkedin.com/in/anjali-panduga-88935a266/">LinkedIn</a></p>
+    </section>
+  );
+}
