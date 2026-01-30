e57f2524d5a81dac29bb4b63f0035d86cd08bef3
ANJALI PANDUGA <pandugaanjali2003@gmail.com>
2026-01-30 19:17:58 +0530

    Education.jsx

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -2,11 +2,25 @@ import { motion } from "framer-motion";
 
 export default function Education() {
   return (
-    <motion.section id="education" whileInView={{ opacity: 1 }} initial={{ opacity: 0 }}>
+    <motion.section
+      id="education"
+      initial={{ opacity: 0, y: 60 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.9 }}
+    >
       <h2>Education</h2>
       <ul>
-        <li>🎓 M.Sc Data Science – VCIW University (CGPA 8.70)</li>
-        <li>🎓 B.Sc Data Science – Osmania University (CGPA 9.20)</li>
+        <li>
+          🎓 <b>M.Sc – Data Science</b><br />
+          VCIW University (2023–2025) <br />
+          CGPA: 8.70 / 10
+        </li>
+        <li style={{ marginTop: "15px" }}>
+          🎓 <b>B.Sc – Data Science</b><br />
+          TSWRD College, Osmania University <br />
+          CGPA: 9.20 / 10
+        </li>
       </ul>
     </motion.section>
   );
