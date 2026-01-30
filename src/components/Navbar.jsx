08c56aa6116364c8a2d2c31c900b52978ad96a0e
ANJALI PANDUGA <pandugaanjali2003@gmail.com>
2026-01-30 19:07:53 +0530

    Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+export default function Navbar() {
+  return (
+    <nav className="nav">
+      <h2>Anjali</h2>
+      <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#education">Education</a></li>
+        <li><a href="#skills">Skills</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#certifications">Certifications</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
