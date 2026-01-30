bacc4979e9c09e4188bafab9babb69208c00ba45
ANJALI PANDUGA <pandugaanjali2003@gmail.com>
2026-01-30 19:11:03 +0530

    Projects.jsx

---
+++ src/components/Projects.jsx
@@ -0,0 +1,19 @@
+export default function Projects() {
+  return (
+    <section id="projects">
+      <h2>Projects</h2>
+
+      <div className="card">
+        <h3>Multilingual Translation App</h3>
+        <a href="https://multilingual-language-translation-app.streamlit.app/">Live</a>
+        <a href="https://github.com/AnjaliPanduga/Language-Translation-App">GitHub</a>
+      </div>
+
+      <div className="card">
+        <h3>Avocado Price Prediction</h3>
+        <a href="https://avocado-price-prediction-using-machine-learning.streamlit.app/">Live</a>
+        <a href="https://github.com/AnjaliPanduga/Avocado-Price-Prediction-Using-Machine-Learning">GitHub</a>
+      </div>
+    </section>
+  );
+}
