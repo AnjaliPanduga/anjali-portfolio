fdeb18f74abb3de81a03745789ef02b6f3288a71
ANJALI PANDUGA <pandugaanjali2003@gmail.com>
2026-01-30 19:11:35 +0530

    Certifications.jsx

---
+++ src/components/Certifications.jsx
@@ -0,0 +1,12 @@
+export default function Certifications() {
+  return (
+    <section id="certifications">
+      <h2>Certifications</h2>
+      <ul>
+        <li>NASSCOM – Data Science Internship</li>
+        <li>HackerRank – SQL Advanced</li>
+        <li>HackerRank – Python</li>
+      </ul>
+    </section>
+  );
+}
